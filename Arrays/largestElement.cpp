c9931700b0d64ffb0bc99dc37b136e6aa6733606
harshitaj25 <rharshita264@gmail.com>
2025-06-15 08:50:07 +0530

    Added optimized solution

--- Arrays/largestElement.cpp
+++ Arrays/largestElement.cpp
@@ -1,4 +1,7 @@
+// Here is the link to the problem statement: https://leetcode.com/problems/largest-element-in-an-array
+// Problem: Find the largest element in an array.
 // Brute Force Approach
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -16,5 +19,36 @@ int main()
     cout << "The Largest element in the array is: " << sortArr(arr1) << endl;
     cout << "The Largest element in the array is: " << sortArr(arr2);
 
+    return 0;
+}
+
+// Here is the optimized approach to find the largest element in an array.
+#include <bits/stdc++.h>
+
+using namespace std;
+int findLargestElement(int arr[], int n)
+{
+
+    int max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    int arr1[] = {2, 5, 1, 3, 0};
+    int n = 5;
+    int max = findLargestElement(arr1, n);
+    cout << "The largest element in the array is: " << max << endl;
+
+    int arr2[] = {8, 10, 5, 7, 9};
+    n = 5;
+    max = findLargestElement(arr2, n);
+    cout << "The largest element in the array is: " << max << endl;
     return 0;
 }
