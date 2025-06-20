0c134cf41846fbc9872018c86a2bbc1b5795a101
harshitaj25 <rharshita264@gmail.com>
2025-06-20 23:06:32 +0530

    Added Moves Zero

---
+++ Arrays/movesZeroToEnd.cpp
@@ -0,0 +1,28 @@
+// Here is the leetcode problem link: https://leetcode.com/problems/move-zeroes/
+// problem statement: Move all zeroes to the end of the array while maintaining the order of non-zero elements.
+
+// Brute Force Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> moveZeros(int n, vector<int> arr)
+{
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+            temp.push_back(arr[i]);
+    }
+
+    int nonZero = temp.size();
+    for (int i = 0; i < nonZero; i++)
+    {
+        arr[i] = temp[i];
+    }
+
+    // fill rest of the cells with 0:
+    for (int i = nonZero; i < n; i++)
+    {
+        arr[i] = 0;
+    }
+    return arr;
